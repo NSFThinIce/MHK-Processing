7fef3a67416f15bd173b41ff740d1b04c07da71f
Christopher Jamieson <shift16@protonmail.com>
2024-06-11 19:53:50 -0400

    Updated the code that cleans the Mohonk Lake ice chart data (MohonkLake-IceOnIceOff-1932-2024.csv). Now, the result is much easier to graph.

--- 02_Scripts/02-MohonkLake-Ice_ChartConstructor-CJ.R
+++ 02_Scripts/02-MohonkLake-Ice_ChartConstructor-CJ.R
@@ -1,32 +1,40 @@
 # Loads the tidyverse packages which are being utilized below
 library(tidyverse)
-library(ggplot2)
+library(lubridate)
 
 # Loads the MohonkLake ice data from the CSV file
-# On other operating systems
-MohonkLake_IceData <- readr::read_csv(file = "./01_Data/IceData/MohonkLake-IceOnIceOff-1932-2024.csv")
+raw_ice_data <- readr::read_csv(file = "./01_Data/IceData/MohonkLake-IceOnIceOff-1932-2024.csv")
 
-# Converts the dates stored in the data set into "date-time objects"
-# Type "?POSIXct" for more information on the formatting
+# Creates a Dataframe with the columns:
+# Ice In -> Date of ice appearing on lake
+# Ice out -> Date of ice receeding on lake
+# Length -> that stores the amount of the days the ice was at the lake before it receded
+# round() is used to prevent fractional days from appearing (They occur because of Daylight Savings)
 
-MohonkLake_IceData_As_DateTimeObjects <- MohonkLake_IceData |>
-    dplyr::mutate(
-        ICEIN_1 = strptime(ICEIN_1, format = "%m/%d/%Y"),
-        ICEOUT_1 = strptime(ICEOUT_1, format = "%m/%d/%Y"),
-        ICEIN_2 = strptime(ICEIN_2, format = "%m/%d/%Y"),
-        ICEOUT_2 = strptime(ICEOUT_2, format = "%m/%d/%Y"),
-        ICEIN_3 = strptime(ICEIN_3, format = "%m/%d/%Y"),
-        ICEOUT_3 = strptime(ICEOUT_3, format = "%m/%d/%Y"),
-    )
+# To do this we must first: Combine all of the data into 2 columns (Ice In & Ice Out)
+ice_data_combined <- tibble(
+  "Ice In" = c(raw_ice_data$ICEIN_1, raw_ice_data$ICEIN_2, raw_ice_data$ICEIN_3),
+  "Ice Out" = c(raw_ice_data$ICEOUT_1, raw_ice_data$ICEOUT_2, raw_ice_data$ICEOUT_3)
+)
 
-# This code appends new columns to the MohonkLake_IceData_As_DateTimeObjects data frame
-# called "Ice length" which represents the amount of time in days of how long it took for
-# the ice to retreat
-MohonkLake_IceData_With_TimeLength <- MohonkLake_IceData_As_DateTimeObjects |>
-    dplyr::mutate(
-        # Ceiling is used to round up to prevent having "fractional" days occur
-        ICE_LENGTH1 = ceiling(difftime(ICEOUT_1, ICEIN_1, units = "days")),
-        ICE_LENGTH2 = ceiling(difftime(ICEOUT_2, ICEIN_2, units = "days")),
-        ICE_LENGTH3 = ceiling(difftime(ICEOUT_3, ICEIN_3, units = "days"))
-    )
+# Then, filter out data with "NA"'s and "No date"'s
+ice_data_filtered <- ice_data_combined |>
+  dplyr::filter(!is.na(`Ice In`) & !is.na(`Ice Out`) & `Ice In` != "No date" & `Ice Out` != "No date")
 
+# Now, there are only dates! Let's convert them to valid dates!
+ice_data_as_dates <- ice_data_filtered |> dplyr::mutate(
+  `Ice In` = lubridate::mdy(`Ice In`),
+  `Ice Out` = lubridate::mdy(`Ice Out`)
+)
+
+# Now, let's add length
+ice_data_with_length <- ice_data_as_dates |> dplyr::mutate(
+  Length = round(difftime(`Ice Out`, `Ice In`, units = "days", tz = "America/New_York"))
+)
+
+# Then, let's sort the dates!!
+ice_data_complete <- ice_data_with_length |>
+  arrange(`Ice In`)
+
+# Now we are complete! Let's view the result...
+View(ice_data_complete)
