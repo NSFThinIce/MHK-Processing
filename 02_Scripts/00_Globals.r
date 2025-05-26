725cc9ed962370ba9d9e6e195d834fc5ccc3bee6
Chris J. <shift16@protonmail.com>
2025-05-26 12:44:16 -0400

    cleanup: Removed base as it's too verbose

--- 02_Scripts/00_Globals.r
+++ 02_Scripts/00_Globals.r
@@ -10,7 +10,7 @@
 #   - 03_Graphs
 #     - ...
 #   - ...
-PATH_TO_DATA <- base::file.path("01_Data")
+PATH_TO_DATA <- file.path("01_Data")
 
 # Path to the data directory if the working directory is the scripts (or any of the other sub-directory) directory
 # - Root
@@ -22,27 +22,27 @@ PATH_TO_DATA <- base::file.path("01_Data")
 #   - 03_Graphs
 #     - ...
 #   - ...
-if (!base::dir.exists(PATH_TO_DATA)) {
-  PATH_TO_DATA <- base::file.path("..", "01_Data")
+if (!dir.exists(PATH_TO_DATA)) {
+  PATH_TO_DATA <- file.path("..", "01_Data")
 }
 
 # If the directory fails to exist in both cases, then stop the program because
 # the program is not running in a supported working directory
-if (!base::dir.exists(PATH_TO_DATA)) {
+if (!dir.exists(PATH_TO_DATA)) {
   stop(paste("This script is being run from ", getwd(), " which is not supported"))
 }
 
 # Directory that holds all of the data pertaining to Mohonk
-MOHONK_DATA_DIR <- base::file.path(PATH_TO_DATA, "MHK_Data")
+MOHONK_DATA_DIR <- file.path(PATH_TO_DATA, "MHK_Data")
 
 # Directory containing unformatted data exported from Kor Software
-KOR_UNFORMATTED_DATA_DIR <- base::file.path(MOHONK_DATA_DIR, "EXO1Sonde", "KorFormat")
+KOR_UNFORMATTED_DATA_DIR <- file.path(MOHONK_DATA_DIR, "EXO1Sonde", "KorFormat")
 
 # CSV Containing all of the exported data from Kor Software
-KOR_UNFORMATTED_DATA_ALL <- base::file.path(KOR_UNFORMATTED_DATA_DIR, "AllExports.csv")
+KOR_UNFORMATTED_DATA_ALL <- file.path(KOR_UNFORMATTED_DATA_DIR, "AllExports.csv")
 
 # Directory containing all of the formatted data from Kor Software and formatted with a script
-KOR_FORMATTED_DATA_DIR <- base::file.path(MOHONK_DATA_DIR, "EXO1Sonde", "DesiredFormat")
+KOR_FORMATTED_DATA_DIR <- file.path(MOHONK_DATA_DIR, "EXO1Sonde", "DesiredFormat")
 
 # Directory containing all of the data from the DO sensor when collecting data from Mohonk
-DO_SENSOR_DATA_DIR <- base::file.path(MOHONK_DATA_DIR, "DOSensor")
+DO_SENSOR_DATA_DIR <- file.path(MOHONK_DATA_DIR, "DOSensor")
